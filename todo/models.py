943fce4f98fdbfe77abed6a6c4f4b0ac1b930c9f
NgiapPuoyKoh <ngiappuoykoh@gmail.com>
2021-05-20 16:41:22 +0000

    Create and register Item model

--- todo/models.py
+++ todo/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 # Create your models here.
 # class inheritance form django model class
+
+
 class Item(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(null=False, blank=False, default=False)
+    # override using name of item entered by user instead of the default
 
-    # override using name of item entered by user instead of the default 
     def __str__(self):
         return self.name
-
